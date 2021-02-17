225b8b9138b9c66e4fadda4db9f5ad77bc3e8bf7
Ted Liu <tedliu@tenafe.com>
2021-02-17 18:08:56 +0800

    add time limit exceeded solution

--- 3.longest-substring-without-repeating-characters.c
+++ 3.longest-substring-without-repeating-characters.c
@@ -9,6 +9,37 @@
 
 int lengthOfLongestSubstring(char * s){
 
+    // Time Limit Exceeded 986/987 cases passed (N/A) 
+
+    int length = strlen(s);
+    int maxlen = 0;
+
+    if (length == 1)
+    {
+        return length;
+    }
+
+    for (int start = 0; start < length-1; start++) {
+        for (int end = start+1; end < length; end++) {
+            int currentlen = 0;
+            char ascii[256] = {0};
+            for (int idx = start; idx <= end; idx++) {
+                int current = s[idx];
+
+                if (ascii[current] == 0) {
+                    ascii[current] = 1;
+                    currentlen++;
+                    if (currentlen > maxlen)
+                        maxlen = currentlen;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+
+    return maxlen;
 }
 // @lc code=end
 
