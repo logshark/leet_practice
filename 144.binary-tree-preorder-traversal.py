36757d40816b85de79362296768696a42576d7b1
logshark <cloud76611@gmail.com>
2022-09-02 13:57:09 +0800

    add binary tree preorder/inorder/postorder traversal

---
+++ 144.binary-tree-preorder-traversal.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=144 lang=python3
+#
+# [144] Binary Tree Preorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        # print(root)
+        if root != None:
+            self.preorder(root, result)
+
+        return result
+
+    def preorder(self, node: TreeNode, result: List[int]) -> None:
+        if node != None:
+            result.append(node.val)
+            self.preorder(node.left, result)
+            self.preorder(node.right, result)
+        else:
+            return
+
+        return
+# @lc code=end
+
